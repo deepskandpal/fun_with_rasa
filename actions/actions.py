22798ee1add84e60f88a48200dc0585c47e7da02
deepanshu.kandpal <deepanshu.kandpal@ril.com>
2021-03-17 11:09:51 +0530

    adding custom action server

--- actions/actions.py
+++ actions/actions.py
@@ -8,20 +8,24 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-
+from rasa_sdk.events import SlotSet
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionHelloWorld(Action):
+class ExplainProducts(Action):
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "explain_products"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
-        dispatcher.utter_message(text="Hello World!")
-
+    def run(self, dispatcher, tracker, domain):
+        product = tracker.get_slot('product')
+        if "credit" in product:
+            dispatcher.utter_message(text = "credit cards are the best")
+        elif "loan" in product:
+             dispatcher.utter_message(text = "loans are great")
+        elif "insuarance" in product:
+             dispatcher.utter_message(text = "insuarance is a subject matter to market risk")
+        else:
+            dispatcher.utter_message(text = "you can know more about {product} on our solutions page ")
         return []
